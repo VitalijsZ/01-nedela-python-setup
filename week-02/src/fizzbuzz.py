754f6156f82428666c3a336f9d4df47de3c989f2
VitalijsZ <vitalijszaharevskis@gmail.com>
2026-03-09 11:49:32 +0200

    feat: add FizzBuzz script with command-line argument N

--- week-02/src/fizzbuzz.py
+++ week-02/src/fizzbuzz.py
@@ -1,7 +1,22 @@
+import sys
+
+# Validacija: parbaudam, vai N ir padots ka arguments
+if len(sys.argv) < 2:
+    print("Please provide N")
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be an integer (number)")
+    sys.exit(1)
+
 output = []
 
-for number in range (1, 16):
+# FizzBuzz
+for number in range (1, N + 1):
     if number % 3 == 0 and number % 5 == 0:
+        # Pirmam kartam parbaudam dalamibu ar abiem
         output.append("FizzBuzz")
     elif number % 3 == 0:
         output.append("Fizz")
@@ -10,7 +25,9 @@ for number in range (1, 16):
     elif number % 7 == 0:
         output.append("Jazz")
     else:
+        # Ja dalamiba neattiecas, pievienojam pasu skaitli
         output.append(str(number))
 
+# Rezultata izvade ka viena rinda ar komatiem
 result = ", ".join(output)
 print(result)
